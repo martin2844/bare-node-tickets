39a77583c612cbd58e75ef00eac83c1ab5bd7726
Martin Chammah <martin2844@gmail.com>
2021-07-20 17:05:06 -0300

    added collection methods, refactored data layer, fixed server headers

--- index.js
+++ index.js
@@ -8,6 +8,36 @@ server.init(logger);
 //Require routes
 require("./routes")
 
+const data = require("./lib/data");
+data.addLogger(logger);
+
+//Create Database collections
+data.createCollection("tickets", (success, error) => {
+    if(success) {
+        console.log("DB Collection: " + success);
+    } else {
+        console.log(error);
+    }
+})
+
+//Creates collection user.
+data.createCollection("users", (success, error) => {
+    if(success) {
+        console.log("DB Collection: " + success);
+    } else {
+        console.log(error);
+    }
+})
+
+//Creates a user
+// data.collection("users").create({
+//     email: "test@test.com",
+//     password: "tester25"
+// })
+
+
+// console.log(data.collection("users").findAll())
+
 process.on('unhandledRejection', (reason, p) => {
     logger.error('exception occurred \n' + JSON.stringify(reason) );
     throw reason;
@@ -18,11 +48,6 @@ process.on('unhandledException', (reason, p) => {
     throw reason;
 });
 
-const data = require("./lib/data");
-
-data.createCollection("/tickets", "tickets", {hello: "world2"}, (err, success) => {
-    console.log(err, success)
-}, logger)
 
 //Server listen
 const PORT = 6969
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,7 +1,21 @@
 const server = require("../../lib/server")
+const data = require("../../lib/data");
 
 server.post("/users", (req, res) => {
     console.log("POST at USERS", req.body)
-    res.status(201).send("User created")
+    try {
+        if(!req.body.email) {
+            throw new Error("an email is needed to create a user")
+        }
+        if(!req.body.password) {
+            throw new Error("a password is needed to create a user")
+        }
+    
+        let newUser = data.collection("users").create(req.body)
+        res.status(201).send(newUser)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
 })
 
